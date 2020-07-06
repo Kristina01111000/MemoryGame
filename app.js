b7dcd0116c24396d3811669443c0ee119c321fd9
Kristina01111000 <64657260+Kristina01111000@users.noreply.github.com>
2020-07-06 14:40:52 -0400

    added card array

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
-
+  const cardArray = [
+    {
+      name: 'blue',
+      img: 'images/blue'
+    },
+    {
+      name: 'green',
+      img: 'images/green'
+    },
+    {
+      name: 'orange',
+      img: 'images/orange'
+    },
+    {
+      name: 'purple',
+      img: 'images/purple'
+    },
+    {
+      name: 'red',
+      img: 'images/red'
+    },
+    {
+      name: 'yellow',
+      img: 'images/yellow'
+    }
+  ]
 
 
 
