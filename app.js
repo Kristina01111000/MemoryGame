f9a2c252028b750452c4f75ade6b85ac143e6327
Kristina01111000 <64657260+Kristina01111000@users.noreply.github.com>
2020-07-09 14:57:54 -0400

    switched image in createboard

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ document.addEventListener('DOMContentLoaded', () => {
   function createBoard() {
     for (let i = 0; i < cardArray.length; i++) {
       var card = document.createElement('img');
-      card.setAttribute('src', 'images/red.png');
+      card.setAttribute('src', 'images/blank.png');
       card.setAttribute('data-id', i);
       //card.addEventListener('click', flipcard)
       grid.appendChild(card);
