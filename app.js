2bde5b5c56278516763ede1eb271649037f2899d
Kristina01111000 <64657260+Kristina01111000@users.noreply.github.com>
2020-07-09 14:34:25 -0400

    finished create board and called it
    doesn't show up on screen

--- app.js
+++ app.js
@@ -50,16 +50,18 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   ]
 
-  const grid = document.querySelector('.grid');
-
+  // create your board
   function createBoard() {
-  for (let i = 0; i < cardArray.length; i++) {
-    var card = document.createElement('img');
-    card.setAttribute('src', 'images/blank.png');
-    card.setAttribute('data-id', i);
+    for (let i = 0; i < cardArray.length; i++) {
+      var card = document.createElement('img');
+      card.setAttribute('src', 'images/red.png');
+      card.setAttribute('data-id', i);
+      //card.addEventListener('click', flipcard)
+      grid.appendChild(card);
+    }
   }
-}
 
 
+createBoard();
   
 });
